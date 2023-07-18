2ef0d39965a665d96f76b515ba20a66713d65c6e
theassembler1 <lewisnoah592@gmail.com>
2023-07-18 18:33:27 -0500

    device

--- src/model/device.rs
+++ src/model/device.rs
@@ -1,11 +1,11 @@
-use super::auth::UserIdQueryExtractor;
+use super::datastructure::DataStructureQueryExtractor;
 use mongodb::bson::doc;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 pub struct DeviceIdQueryExtractor {
     #[serde(flatten)]
-    pub user_id_query_extractor: UserIdQueryExtractor,
+    pub data_structure_query_extractor: DataStructureQueryExtractor,
     #[serde(rename(serialize = "deviceId", deserialize = "deviceId"))]
     pub device_id: String,
 }

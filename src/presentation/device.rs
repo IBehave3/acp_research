e0fefe7c67cdfafdc481e850cd00e002f1c56a52
theassembler1 <lewisnoah592@gmail.com>
2023-07-18 18:33:27 -0500

    device

--- src/presentation/device.rs
+++ src/presentation/device.rs
@@ -1,14 +1,42 @@
-
-
 use crate::model::device::DeviceIdQueryExtractor;
 
+use crate::model::auth::IdMapping;
 use actix_web::{delete, post, web::Query, HttpResponse, Responder, Result};
-
 
 #[post("/add-device")]
 pub async fn add_device_get_handler(
-    _query: Query<DeviceIdQueryExtractor>,
+    query: Query<DeviceIdQueryExtractor>,
 ) -> Result<impl Responder> {
+    let id_mapping = IdMapping::get_id_mapping_by_user_id(
+        &query
+            .data_structure_query_extractor
+            .user_id_query_extractor
+            .user_id,
+    )
+    .await?;
+
+    // NOTE: checking that the user exists
+    let &mut id_mapping = match id_mapping {
+        Some(ref mut id_mapping) => id_mapping,
+        None => return Ok(HttpResponse::NotFound().finish()),
+    };
+
+    // NOTE: checking that the data structure exists
+    let &mut data_structure_device_id_mapping = match id_mapping
+        .data_structure_device_id_mapping
+        .iter()
+        .find(|x| x.data_structure_id == query.data_structure_query_extractor.data_structure_id)
+    {
+        Some(ref mut data_structure_device_id_mapping) => data_structure_device_id_mapping,
+        None => return Ok(HttpResponse::NotFound().finish()),
+    };
+
+    // NOTE: checking that the device id does not exists
+    match data_structure_device_id_mapping.device_ids {
+        Some(ref mut device_ids) => device_ids.retain(|device_id| *device_id != query.device_id),
+        None => return Ok(HttpResponse::NotFound().finish()),
+    }
+
     Ok(HttpResponse::Ok().finish())
 }
 

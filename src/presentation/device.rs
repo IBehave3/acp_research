8866fef21e65148e0b6f3cee51735007d52e6612
theassembler1 <lewisnoah592@gmail.com>
2023-07-18 20:01:07 -0500

    remove device

--- src/presentation/device.rs
+++ src/presentation/device.rs
@@ -1,4 +1,4 @@
-use crate::model::device::DeviceIdQueryExtractor;
+use crate::{infra::collection::BaseCollection, model::device::DeviceIdQueryExtractor};
 
 use crate::model::auth::IdMapping;
 use actix_web::{delete, post, web::Query, HttpResponse, Responder, Result};
@@ -7,7 +7,7 @@ use actix_web::{delete, post, web::Query, HttpResponse, Responder, Result};
 pub async fn add_device_get_handler(
     query: Query<DeviceIdQueryExtractor>,
 ) -> Result<impl Responder> {
-    let id_mapping = IdMapping::get_id_mapping_by_user_id(
+    let mut id_mapping = IdMapping::get_id_mapping_by_user_id(
         &query
             .data_structure_query_extractor
             .user_id_query_extractor
@@ -16,26 +16,27 @@ pub async fn add_device_get_handler(
     .await?;
 
     // NOTE: checking that the user exists
-    let &mut id_mapping = match id_mapping {
-        Some(ref mut id_mapping) => id_mapping,
+    let id_mapping = match &mut id_mapping {
+        Some(id_mapping) => id_mapping,
         None => return Ok(HttpResponse::NotFound().finish()),
     };
 
-    // NOTE: checking that the data structure exists
-    let &mut data_structure_device_id_mapping = match id_mapping
+    // NOTE: checking that the data structure and removin the given device_id if it exists
+    match id_mapping
         .data_structure_device_id_mapping
-        .iter()
+        .iter_mut()
         .find(|x| x.data_structure_id == query.data_structure_query_extractor.data_structure_id)
     {
-        Some(ref mut data_structure_device_id_mapping) => data_structure_device_id_mapping,
+        Some(data_structure_device_id_mapping) => {
+            match &mut data_structure_device_id_mapping.device_ids {
+                Some(device_ids) => device_ids.retain(|device_id| *device_id != query.device_id),
+                None => return Ok(HttpResponse::NotFound().finish()),
+            }
+        }
         None => return Ok(HttpResponse::NotFound().finish()),
     };
 
-    // NOTE: checking that the device id does not exists
-    match data_structure_device_id_mapping.device_ids {
-        Some(ref mut device_ids) => device_ids.retain(|device_id| *device_id != query.device_id),
-        None => return Ok(HttpResponse::NotFound().finish()),
-    }
+    IdMapping::replace(id_mapping._id, &id_mapping).await?;
 
     Ok(HttpResponse::Ok().finish())
 }

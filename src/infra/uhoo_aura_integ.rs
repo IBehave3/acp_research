dcb5e6d345d12c0fe20f41bfefabb4b69a1ec38e
theassembler1 <lewisnoah592@gmail.com>
2023-08-13 17:32:51 -0500

    removed unwraps and handled errors for polling sensors

--- src/infra/uhoo_aura_integ.rs
+++ src/infra/uhoo_aura_integ.rs
@@ -1,9 +1,11 @@
 use chrono::Utc;
 use reqwest::{Client, Response};
 use serde::{Serialize, Deserialize};
+use core::panic;
 use std::time::Duration;
 use std::thread;
 use mongodb::{bson, bson::doc, bson::oid::ObjectId};
+use log::{info, error};
 
 use crate::{model::{auth::IdMapping, push_data::PushData}, infra::collection::BaseCollection};
 
@@ -26,7 +28,8 @@ pub async fn get_token(client_secret: &str) -> Result<AccessTokenResponse, Box<d
                     .send()
                     .await?;
 
-    let access_token_response: AccessTokenResponse = serde_json::from_slice(&response.bytes().await.unwrap()[..]).unwrap();
+    let bytes = &response.bytes().await?[..];
+    let access_token_response: AccessTokenResponse = serde_json::from_slice(bytes)?;
 
     Ok(access_token_response)
 }
@@ -46,33 +49,100 @@ pub async fn get_device_data(token: &str, dev_serial: &str) -> Result<Response, 
 
 pub fn start_uhoo_aura_poll() {
     thread::spawn(move || {
-        let rt = tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build().unwrap();
+        let rt = match tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build() {
+                Ok(rt) => rt,
+                Err(err) => {
+                    error!("{err}");
+                    panic!("unable to start uhoo_aura poll");
+                }
+            };
 
         loop {
-            let id_mappings = rt.block_on(IdMapping::get_airthings_users()).unwrap();
+            let id_mappings = match rt.block_on(IdMapping::get_airthings_users()) {
+                Ok(id_mappings) => id_mappings,
+                Err(err) => {
+                    error!("{err}");
+                    continue;
+                },
+            };
 
             for id_mapping in id_mappings {
                 for device_id_mapping in id_mapping.data_structure_device_id_mapping {
                     if device_id_mapping.data_structure_id.eq("uhoo_aura") {
-                        let client_secret = device_id_mapping.auth.as_ref().unwrap().client_secret.clone().unwrap();
+                        let device_id_mapping_ref = match device_id_mapping.auth.as_ref() {
+                            Some(device_id_mapping_ref) => device_id_mapping_ref,
+                            None => {
+                                error!("no uhoo_ara data structure found for user");
+                                continue;
+                            }
+                        };
+                        let client_secret = match device_id_mapping_ref.client_secret.clone() {
+                            Some(client_secret) => client_secret,
+                            None => {
+                                error!("no client secret found for uhoo_aura");
+                                continue;
+                            }
+                        };
 
-                        for device_id in device_id_mapping.device_ids.unwrap() {
-                            let token = rt.block_on(get_token(&client_secret)).unwrap();
-                            let response = rt.block_on(get_device_data(&token.access_token, &device_id)).unwrap();
+                        let device_ids = match device_id_mapping.device_ids {
+                            Some(device_ids) => device_ids,
+                            None => {
+                                error!("no device ids found for uhoo_aura");
+                                continue;
+                            } 
+                        };
+
+                        for device_id in device_ids {
+                            let token = match rt.block_on(get_token(&client_secret)) {
+                                Ok(token) => token,
+                                Err(err) => {
+                                    error!("{err}");
+                                    continue;
+                                }
+                            };
+                            let response = match rt.block_on(get_device_data(&token.access_token, &device_id)) {
+                                Ok(response) => response,
+                                Err(err) => {
+                                    error!("{err}");
+                                    continue;
+                                }
+                            };
 
                             if response.status() == 200 {
-                                println!("writing uhoo_aura to push_data");
-                                let bytes = rt.block_on(response.bytes()).unwrap();
-                                rt.block_on(PushData::add(PushData {
+                                let user_ref_id = id_mapping._id;
+                                info!("uhoo_aura writing data for (user_id, device_id): ({user_ref_id}, {device_id})");
+
+                                let bytes = match rt.block_on(response.bytes()) {
+                                    Ok(bytes) => bytes,
+                                    Err(err) => {
+                                        error!("{err}");
+                                        continue;
+                                    }
+                                };
+                                let json = match serde_json::from_slice(&bytes[..]) {
+                                    Ok(json) => json,
+                                    Err(err) => {
+                                        error!("{err}");
+                                        continue;
+                                    }
+                                };
+                                
+                                match rt.block_on(PushData::add(PushData {
                                     _id: ObjectId::new(),
                                     device_id: Some(device_id),
                                     created_at: bson::DateTime::from_chrono(Utc::now()),
                                     data_structure_id: "uhoo_aura".to_string(),
-                                    id_mapping_ref_id: id_mapping._id,
-                                    data: serde_json::from_slice(&bytes[..]).unwrap(),
-                                })).unwrap();
+                                    id_mapping_ref_id: user_ref_id,
+                                    data: json,
+                                })) {
+                                    Ok(_) => (),
+                                    Err(err) => {
+                                        error!("{err}");
+                                        continue;
+                                    }
+                                }
                             }
                         }
                     }
